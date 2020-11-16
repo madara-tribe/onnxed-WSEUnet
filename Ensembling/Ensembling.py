088288cc45cc3a0b9f7b64365679503c28bde28a
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2020-11-16 14:00:56 +0900

    Update Ensembling.py

--- Ensembling/Ensembling.py
+++ Ensembling/Ensembling.py
@@ -14,25 +14,16 @@ import cv2
 from models.WideResUnet.WideResBlock_UNet import create_model
 
 
-def call_model(weight_path):
-    H=256
-    W=256
-    model_input = Input(shape=(H, W, 3))
-    model = create_model(model_input)
-    model.load_weights(weight_path)
-    return model, model_input
-    
-# ensemble model
-def create_ensemble_model(models):
-    model1, model_input = call_model(weight_path1)
-    model2, _ = call_model(weight_path2)
+def create_ensemble_model(model1, model2):
+    inputs_ = model1.inputs
     models = [model1, model2]
     outputs = [model.outputs[0] for model in models]
     o = Average()(outputs)
-
-    ensemble_model = Model(model_input, o, name='ensemble')
+    ensemble_model = Model(inputs_, o, name='ensemble')
     ensemble_model.summary()
     return ensemble_model
+
+#ens_model = create_ensemble_model(fmodel, fmodel2)
 
 #predictions = ensemble_model.predict(test_img)
 #print(predictions.shape)
