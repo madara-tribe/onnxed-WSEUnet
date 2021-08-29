d8fc9b66bfe5eb037fa001b20cff1fef22bf1b27
萩原辰哉 <hagiharatatsuya@hagiharatatsuyanoMacBook-Air.local>
2021-08-30 07:45:50 +0900

    update

--- option_parser.py
+++ option_parser.py
@@ -2,9 +2,11 @@ from argparse import ArgumentParser
 
 def get_option():
     argparser = ArgumentParser()
-    argparser.add_argument('-e', '--epoch', type=int, default=10,
+    argparser.add_argument('-e', '--epoch', type=int, default=50,
                            help='Specify number of epoch')
     argparser.add_argument('--channel', type=int, default=4)
-    argparser.add_argument('--batch_size', type=int, default=4)
+    argparser.add_argument('--batch_size', type=int, default=3)
     argparser.add_argument('--num_cls', type=int, default=1)
+    argparser.add_argument('--weight_dir', type=str, default='weights')
+    argparser.add_argument('--model_type', type=str, default='se')
     return argparser.parse_args()
