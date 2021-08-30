ae3d01ceadeee11b9dbcb0307786764377d24e50
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2021-08-31 07:53:08 +0900

    Update train.py

--- train.py
+++ train.py
@@ -12,11 +12,9 @@ from tensorflow.keras.callbacks import *
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 from losses.bce_dice_loss import bce_dice_loss, dice_loss
-from losses.focal_loss import focal_loss
-from fbeta_score import binary_fbeta
+from fbeta_score import binary_fbeta, fbeta_loss
 from Augmentation import sift_angle, flip, random_eraser
 from models.WSEUnet import create_model
-from models.res50_unet import ResNet_Unet 
 from option_parser import get_option
 H=448
 W=448
@@ -28,21 +26,18 @@ def gene_flow_inputs(datagen, x_image, y_train, batch=2):
         yield batch_image, batch_mask
 
 
-def load_data(config=None, split_n=2):
+def load_data(config=None):
     generators = ImageDataGenerator(rotation_range=10,
                                     horizontal_flip=True,
                                     #vertical_flip=True,
-                                    fill_mode='constant', 
-                                    validation_split=0.2)
+                                    fill_mode='constant')
                                     #preprocessing_function=random_eraser.get_random_eraser(v_l=0, v_h=1))
     imgs, annos = np.load('data/sar_img.npy'), np.load('data/sar_anno.npy')
-    
+    N = 5
     imgs, annos = imgs.astype(np.float32), annos.astype(np.float32)
-    #imgs1, annos1 = imgs[:, ::-1], annos[:, ::-1]
-    #imgs= np.vstack([imgs, imgs1])
-    #annos = np.vstack([annos, annos1])
-    X_train, y_train = imgs[split_n:], annos[split_n:]
-    X_val, y_val = imgs[:split_n], annos[:split_n]
+    #annos = annos.reshape(len(annos), H, W, 1)
+    X_train, y_train = imgs[N:], annos[N:]
+    X_val, y_val = imgs[:N], annos[:N]
     print(imgs.shape, annos.shape)
     training = gene_flow_inputs(generators, X_train, y_train, batch=config.batch_size)
     return training, X_train, X_val, y_val
@@ -50,12 +45,8 @@ def load_data(config=None, split_n=2):
 def load_model(config=None, weight_path=None):
     adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, decay=0.0)
     sgd = SGD(lr=1E-2, decay=5**(-4), momentum=0.9, nesterov=True)
-    if config.model_type=='se':
-        models = create_model(config.num_cls, input_shape=(H, W, 3))
-        models.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
-    elif config.model_type=='resnet':
-        models = ResNet_Unet(nClasses=config.num_cls, input_shape=(H, W, 3))
-        models.compile(optimizer=adam, loss=bce_dice_loss, metrics=[binary_fbeta])
+    models = create_model(config.num_cls, input_shape=(H, W, 3))
+    models.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
     if weight_path:
         models.load_weights(os.path.join('weights', weight_path))
     models.summary()
@@ -63,7 +54,7 @@ def load_model(config=None, weight_path=None):
 
 
 def train(cfg):
-    training, X_train, X_val, y_val = load_data(cfg, split_n=5)
+    training, X_train, X_val, y_val = load_data(cfg)
     models = load_model(config=cfg, weight_path=None)
      
     checkpoint_path = "weights/cp-{epoch:02d}.hdf5"
@@ -91,3 +82,4 @@ if __name__=='__main__':
     cfg = get_option()
     os.makedirs(cfg.weight_dir, exist_ok=True)
     train(cfg)
+
