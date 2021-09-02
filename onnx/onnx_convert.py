38455a8cd099dbdc4702a92450f957d4b068655b
萩原辰哉 <hagiharatatsuya@hagiharatatsuyanoMacBook-Air.local>
2021-09-02 15:12:17 +0900

    update

--- onnx/onnx_convert.py
+++ onnx/onnx_convert.py
@@ -6,13 +6,14 @@ import keras2onnx
 from keras2onnx import convert_keras
 import onnxruntime
 import onnx
+from models.mish_Resnet_RS.Resnet_RS import create_model as create_mishrs_model
 from models.SEUNet.WSEUnet import create_model
 from models.efficientnet.efficientunet import _get_efficient_unet
 from models.efficientnet.efficientnet import get_efficientnet_b5_encoder
 from option_parser import get_option
 
 weight_dir = '../weights'
-weight_name = 'cp-04.hdf5'
+weight_name = 'cp-06.hdf5'
 OUTPUT_ONNX_MODEL_NAME = 'unet.onnx'
 H=448
 W=448
@@ -20,6 +21,8 @@ C=3
 def load_model(config=None, weight_path=None):
     if config.model_type=='se':
         models = create_model(config.num_cls, input_shape=(H, W, C))
+    elif config.model_type=='rs_mish':
+        models = create_mishrs_model(config.num_cls, input_shape=(H, W, C))
     elif config.model_type=='efficientunet':
         encoder = get_efficientnet_b5_encoder(input_shape=(H, W, C), pretrained=True)
         models = _get_efficient_unet(encoder, out_channels=config.num_cls,
